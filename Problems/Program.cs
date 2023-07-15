206540bc35a75e3e9322d1cb397f415630b59213
Fkyeah <dimx63@mail.ru>
2023-07-15 23:06:30 +0300

    Roman To Integer Solution

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -6,13 +6,19 @@ var twoSumResponse = twoSum.TwoSum2(twoSum.nums2, 6);
 //Problems.TwoSum.ResponseOutput.Write(twoSumResponse);
 
 #endregion
-
 #region PalindromeNumber
 
 var inPalindrome = new Problems.PalindromeNumber.Solution();
 var inPalindromeResponse = inPalindrome.IsPalindrome4(inPalindrome.x);
-Problems.PalindromeNumber.ResponseOutput.Write(inPalindromeResponse);
+//Problems.PalindromeNumber.ResponseOutput.Write(inPalindromeResponse);
 
 #endregion
+#region RomanToInteger
+
+var romanToInteger = new Problems.RomanToInteger.Solution();
+var romanToIntegerResponse = romanToInteger.RomanToInt(romanToInteger.s3);
+Problems.RomanToInteger.ResponseOutput.Write(romanToIntegerResponse);
+#endregion
+
 
 
