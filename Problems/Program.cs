1100941c6fb649d027451cc49a23a6d6c136a077
Dmitry Tikhonov <73015112+Fkyeah@users.noreply.github.com>
2023-07-15 21:12:30 +0400

    Program region (#2)

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -1,4 +1,11 @@
-﻿var twoSum = new Problems.Two_Sum.Solution();
+﻿
+#region TwoSum
+
+var twoSum = new Problems.Two_Sum.Solution();
 
 var response = twoSum.TwoSum2(twoSum.nums2, 6);
 Problems.Two_Sum.Response.Output(response);
+
+#endregion
+
+
