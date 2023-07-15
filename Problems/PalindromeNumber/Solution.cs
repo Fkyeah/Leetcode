07807c38f9e4b3324056505b398be5aa29ea9ff9
Fkyeah <dimx63@mail.ru>
2023-07-15 20:40:18 +0300

    First solution

---
+++ Problems/PalindromeNumber/Solution.cs
@@ -0,0 +1,41 @@
+﻿namespace Problems.PalindromeNumber
+{
+    public class Solution
+    {
+        public int x = 121;
+        public int x2 = -121;
+        public int x3 = 10;
+
+        /// Runtime 73ms, Memory - 32.74mb
+        public bool IsPalindrome(int x)
+        {
+            var digitsArray = x.ToString().ToCharArray();
+
+            return digitsArray[0] == digitsArray[digitsArray.Length - 1]
+                && IsEqualArrays(digitsArray, digitsArray.Reverse());
+        }
+
+        private bool IsEqualArrays(IEnumerable<char> array1, IEnumerable<char> array2)
+        {
+            if (array1.Count() != array2.Count())
+                return false;
+
+            var firstEnumerator = array1.GetEnumerator();
+            var secondEnumerator = array2.GetEnumerator();
+
+            for (int i = 0; i < array1.Count(); i++)
+            {
+                firstEnumerator.MoveNext();
+                secondEnumerator.MoveNext();
+
+                var first = firstEnumerator.Current;
+                var second = secondEnumerator.Current;
+
+                if (!first.Equals(second))
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
--- Problems/Two_Sum/ResponseOutput.cs
+++ Problems/Two_Sum/ResponseOutput.cs
@@ -1,8 +1,8 @@
 ﻿namespace Problems.Two_Sum
 {
-    public static class Response
+    public static class ResponseOutput
     {
-        public static void Output(int[] response)
+        public static void Write(int[] response)
         {
             for (int i = 0; i < response.Length; i++)
             {
