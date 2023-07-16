9775fe83eb9f1fdef2d0167b85fdd7bdf3f62b24
Fkyeah <dimx63@mail.ru>
2023-07-16 18:24:21 +0300

    Remove duplicates

---
+++ Problems.Tests/Easy/RemoveDuplicatesTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problems.Easy;
+
+namespace Problems.Tests.Easy
+{
+    [TestClass]
+    public class RemoveDuplicatesTests
+    {
+        private static RemoveDuplicatesSolution _solution;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _solution = new RemoveDuplicatesSolution();
+        }
+
+        [DataTestMethod]
+        [DataRow(new int[] { 1, 1, 2 }, new int[] { 1, 2 })]
+        [DataRow(new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 }, new int[] { 0, 1, 2, 3, 4})]
+        public void First_Solution_Tests(int[] nums, int[] expectedResult)
+        {
+            var expectedCount = expectedResult.Length;
+
+            var actualResult = _solution.RemoveDuplicates(nums);
+
+            Assert.AreEqual(expectedCount, actualResult);
+
+            for (int i = 0; i < expectedCount; i++)
+            {
+                Assert.AreEqual(expectedResult[i], nums[i]);
+            }
+        }
+    }
+}
---
+++ Problems/Easy/RemoveDuplicatesSolution.cs
@@ -0,0 +1,24 @@
+﻿namespace Problems.Easy
+{
+    public class RemoveDuplicatesSolution
+    {
+        /// Runtime - 148ms, Memory - 46.38mb
+        public int RemoveDuplicates(int[] nums)
+        {
+            int i = 1;
+
+            foreach (int number in nums)
+            {
+                if (nums[i - 1] != number) 
+                    nums[i++] = number;
+            }
+
+            return i;
+        }
+
+        public int RemoveDuplicates2(int[] nums)
+        {
+            return nums.Distinct().Count();
+        }
+    }
+}
