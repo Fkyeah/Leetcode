4359bbdfe644a3196a3c62d7ce486f7834b51d30
Dmitry Tikhonov <73015112+Fkyeah@users.noreply.github.com>
2023-07-17 22:46:45 +0400

    Merge two sorted lists (#18)
    * Medion of two sorted arrays
    
    * solution and tests

---
+++ Problems/Easy/MergeTwoSortedListsSolution.cs
@@ -0,0 +1,44 @@
+﻿using Problems.Common;
+
+namespace Problems.Easy
+{
+    public class MergeTwoSortedListsSolution
+    {
+        public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            var nodeList = GetQueueFromNode(new List<int>(), list1);
+            nodeList = GetQueueFromNode(nodeList, list2);
+
+
+            return GetResultNode(nodeList.OrderBy(x => x).ToList());
+
+        }
+
+        private ListNode GetResultNode(List<int> resultList)
+        {
+            if (resultList is null || resultList.Count == 0)
+                return null;
+
+            var currentValue = resultList[0];
+            resultList.RemoveAt(0);
+
+            return new ListNode(
+                currentValue,
+                resultList.Count == 0
+                    ? null
+                    : GetResultNode(resultList));
+        }
+
+        private List<int> GetQueueFromNode(List<int> list, ListNode listNode)
+        {
+            if (listNode is null)
+                return list;
+
+            list.Add(listNode.val);
+
+            return listNode.next is null
+                ? list
+                : GetQueueFromNode(list, listNode.next);
+        }
+    }
+}
