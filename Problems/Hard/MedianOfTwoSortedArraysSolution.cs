1554b54071d851797aba3b056f0a515af2e2f900
Fkyeah <dimx63@mail.ru>
2023-07-17 21:11:48 +0300

    Medion of two sorted arrays

---
+++ Problems/Hard/MedianOfTwoSortedArraysSolution.cs
@@ -0,0 +1,23 @@
+﻿namespace Problems.Hard
+{
+    public class MedianOfTwoSortedArraysSolution
+    {
+        /// Runtime - 114ms. Memory - 54.21mb
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            var mergedArray = nums1.Concat(nums2).OrderBy(x => x).ToArray();
+
+            if (mergedArray.Length % 2 == 0)
+            {
+                var n1 = mergedArray[(mergedArray.Length / 2) - 1];
+                var n2 = mergedArray[(mergedArray.Length / 2)];
+
+                return (((double)n1 + n2) / 2);
+            }
+            else
+            {
+                return mergedArray[mergedArray.Length / 2];
+            }
+        }
+    }
+}
