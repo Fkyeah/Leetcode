d3244f6f9548d9e4682f5d4c2871c494976b5c09
Dmitry Tikhonov <73015112+Fkyeah@users.noreply.github.com>
2023-07-17 16:53:54 +0400

    Add two numbers 2 (#15)
    * Add Two Numbers II
    
    * refactoring
    
    * code style
    
    * Summary

---
+++ Problems/Medium/AddTwoNumbersIISolution.cs
@@ -0,0 +1,72 @@
+﻿using Problems.Common;
+
+namespace Problems.Medium
+{
+    public class AddTwoNumbersIISolution
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            var stackL1 = GetStackFromNode(new Stack<int>(), l1);
+            var stackL2 = GetStackFromNode(new Stack<int>(), l2);
+            
+            var resultStack = stackL1.Count > stackL2.Count
+                ? GetResultStack(stackL1, stackL2)
+                : GetResultStack(stackL2, stackL1);
+            
+            return GetResultNode(resultStack);
+        }
+
+        private Stack<int> GetResultStack(Stack<int> stackL1, Stack<int> stackL2)
+        {
+            var reminder = 0;
+            var resultStack = new Stack<int>();
+
+            while (stackL1.TryPop(out var valueL1))
+            {
+                var isSuccessL2Pop = stackL2.TryPop(out var valueL2);
+
+                var resultValue = isSuccessL2Pop 
+                    ? valueL1 + valueL2 + reminder
+                    : valueL1 + reminder;
+
+                if (resultValue >= 10)
+                {
+                    reminder = resultValue / 10;
+                    resultStack.Push(resultValue % 10);
+                }
+                else
+                {
+                    resultStack.Push(resultValue);
+                    reminder = 0;
+                }
+            }
+
+            if (reminder > 0)
+            {
+                resultStack.Push(reminder);
+            }
+                
+
+            return resultStack;
+        }
+
+        private ListNode GetResultNode(Stack<int> resultStack)
+        {
+            return new ListNode(
+                resultStack.Pop(),
+                resultStack.Count == 0
+                    ? null
+                    : GetResultNode(resultStack));
+        }
+
+        private Stack<int> GetStackFromNode(Stack<int> stack, ListNode listNode)
+        {
+            stack.Push(listNode.val);
+
+            return listNode.next is null
+                ? stack
+                : GetStackFromNode(stack, listNode.next);
+
+        }
+    }
+}
