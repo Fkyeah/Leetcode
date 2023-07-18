c1074cd364ae8c1d177953660a887a1ca5745f22
Dmitry Tikhonov <73015112+Fkyeah@users.noreply.github.com>
2023-07-18 09:19:28 +0400

    Find substring index in string (#19)
    * Regular expressions solution
    
    * rename test class

---
+++ Problems/Easy/FindTheIndexOfTheFirstOccurrenceInStringSolution.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Problems.Easy
+{
+    public class FindTheIndexOfTheFirstOccurrenceInStringSolution
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            return haystack.IndexOf(needle);
+        }
+
+        public int StrStr2(string haystack, string needle)
+        {
+            var match = Regex.Match(haystack, needle);
+
+            return !match.Success
+                ? -1
+                : match.Index;
+        }
+    }
+}
