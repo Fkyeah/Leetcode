75583a80c210a794289ef5e7a978c3dee18301fe
Fkyeah <dimx63@mail.ru>
2023-07-18 09:31:23 +0300

    recursion binary search

---
+++ Problems.Tests/Easy/SearchInsertPositionTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problems.Easy;
+
+namespace Problems.Tests.Easy
+{
+    [TestClass]
+    public class SearchInsertPositionTests
+    {
+        private static SearchInsertPositionSolution _solution;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _solution = new SearchInsertPositionSolution();
+        }
+
+        [DataTestMethod]
+        [DataRow(new int[] { 1, 3, 5, 6 }, 5, 2)]
+        [DataRow(new int[] { 1, 3, 5, 6 }, 2, 1)]
+        [DataRow(new int[] { 1, 3, 5, 6 }, 7, 4)]
+        [DataRow(new int[] { 1, 3, 5, 6 }, 0, 0)]
+        [DataRow(new int[] { 1, 3 }, 4, 2)]
+        public void Solution_Tests(int[] nums, int target, int expectedResult)
+        {
+            var actualResult = _solution.SearchInsert(nums, target);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
---
+++ Problems/Easy/SearchInsertPositionSolution.cs
@@ -0,0 +1,38 @@
+﻿namespace Problems.Easy
+{
+    public class SearchInsertPositionSolution
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            var index = BinarySearch(nums, target, 0, nums.Length);
+
+            return index;
+        }
+
+        private int BinarySearch(int[] nums, int target, int left, int right)
+        {
+            int mid = left + (right - left) / 2;
+
+            if (left >= right)
+            {
+                return right;
+            }
+                
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+                
+            if (right - left == 1)
+            {
+                return target > nums[left]
+                    ? right
+                    : left;
+            }
+
+            return nums[mid] > target
+                ? BinarySearch(nums, target, left, mid)
+                : BinarySearch(nums, target, mid + 1, right);
+        }
+    }
+}
