84f51f7702fb0393f4f72fa95f1ed7f93f3e1b2f
Fkyeah <dimx63@mail.ru>
2023-07-18 08:17:44 +0300

    Regular expressions solution

---
+++ Problems.Tests/Easy/FindTheIndexOfTheFirstOccurrenceInString.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problems.Easy;
+
+namespace Problems.Tests.Easy
+{
+    [TestClass]
+    public class FindTheIndexOfTheFirstOccurrenceInString
+    {
+        private static FindTheIndexOfTheFirstOccurrenceInStringSolution _solution;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _solution = new FindTheIndexOfTheFirstOccurrenceInStringSolution();
+        }
+
+        [DataTestMethod]
+        [DataRow("sadbutsad", "sad", 0)]
+        [DataRow("leetcode", "leeto", -1)]
+        public void First_Solution_Tests(string haystack, string needle, int expectedResult)
+        {
+            var actualResult = _solution.StrStr(haystack, needle);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [DataTestMethod]
+        [DataRow("sadbutsad", "sad", 0)]
+        [DataRow("leetcode", "leeto", -1)]
+        public void Second_Solution_Tests(string haystack, string needle, int expectedResult)
+        {
+            var actualResult = _solution.StrStr2(haystack, needle);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+    }
+}
