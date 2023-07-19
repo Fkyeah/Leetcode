250a6c2cff87181320fe9222b3c4f9788811fc33
Dmitry Tikhonov <73015112+Fkyeah@users.noreply.github.com>
2023-07-19 19:41:12 +0400

    Pascal triangle (#21)
    * Pascal's Triangle
    
    * Pascal's Triangle 2 (#20)

---
+++ Problems/Easy/PascalTriangleIISolution.cs
@@ -0,0 +1,28 @@
+﻿namespace Problems.Easy
+{
+    public class PascalTriangleIISolution
+    {
+        public IList<int> GetRow(int rowIndex)
+        {
+            var numRows = rowIndex + 1;
+            int[] prevArray = new int[1] { 1 };
+            for (int i = 1; i < numRows; i++)
+            {
+                var currArray = new int[i + 1];
+
+                currArray[0] = 1;
+                currArray[currArray.Length - 1] = 1;
+
+                for (int j = 1; j <= (currArray.Length - 1) / 2; j++)
+                {
+                    currArray[j] = prevArray[j - 1] + prevArray[j];
+                    currArray[currArray.Length - 1 - j] = prevArray[j - 1] + prevArray[j];
+                }
+
+                prevArray = currArray;
+            }
+
+            return prevArray.ToList();
+        }
+    }
+}
