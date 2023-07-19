2478556fa21091b36a4a7aba34d965a3a5dc8632
Fkyeah <dimx63@mail.ru>
2023-07-19 18:05:54 +0300

    Pascal's Triangle

---
+++ Problems.Tests/Easy/PascalTriangleTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problems.Easy;
+
+namespace Problems.Tests.Easy
+{
+    [TestClass]
+    public class PascalTriangleTests
+    {
+        private static PascalTriangleSolution _solution;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _solution = new PascalTriangleSolution();
+        }
+
+        [DataTestMethod]
+        [DataRow(5)]
+        public void Solution_Tests(int numRows)
+        {
+            var expectedResult = new int[][] { new int[] { 1 }, new int[] { 1, 1 }, new int[] { 1, 2, 1 }, new int[] { 1, 3, 3, 1 }, new int[] { 1, 4, 6, 4, 1 } };
+
+            var actualResult = _solution.Generate(numRows);
+
+            for (int i = 0; i < expectedResult.Length; i++)
+            {
+                for (int j = 0; j < expectedResult[i].Length; j++)
+                {
+                    Assert.AreEqual(expectedResult[i][j], actualResult[i][j]);
+                }
+            }
+        }
+    }
+}
---
+++ Problems/Easy/PascalTriangleSolution.cs
@@ -0,0 +1,34 @@
+﻿namespace Problems.Easy
+{
+    public class PascalTriangleSolution
+    {
+        public IList<IList<int>> Generate(int numRows)
+        {
+            List<IList<int>> result = new List<IList<int>>();
+            
+            for (int i = 1; i <= numRows; i++)
+            {
+                var arr = new int[i];
+                arr[0] = 1;
+                arr[arr.Length - 1] = 1;
+
+                
+
+                if (i > 2)
+                {
+                    for (int j = 1; j <= (arr.Length - 1) / 2; j++)
+                    {
+                        var prevArr = result.LastOrDefault().ToArray();
+
+                        arr[j] = prevArr[j - 1] + prevArr[j];
+                        arr[arr.Length - 1 - j] = prevArr[j - 1] + prevArr[j];
+                    }
+                }
+
+                result.Add(arr.ToList());
+            }
+
+            return result;
+        }
+    }
+}
