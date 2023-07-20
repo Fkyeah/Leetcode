f700eaca9052ad176f65a3cc1df87b0f69a08367
Fkyeah <dimx63@mail.ru>
2023-07-20 23:10:46 +0300

    Reverse Integer

---
+++ Problems.Tests/Medium/ReverseIntegerTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problems.Medium;
+
+namespace Problems.Tests.Medium
+{
+    [TestClass]
+    public class ReverseIntegerTests
+    {
+        private static ReverseIntegerSolution _solution;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _solution = new ReverseIntegerSolution();
+        }
+
+        [DataTestMethod]
+        [DataRow(1534236469, 0)]
+        [DataRow(-123, -321)]
+        public void Solution_Tests(int x, int expectedResult)
+        {
+            var actualResult = _solution.Reverse(x);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
---
+++ Problems/Medium/ReverseIntegerSolution.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+namespace Problems.Medium
+{
+    public class ReverseIntegerSolution
+    {
+        public int Reverse(int x)
+        {
+            StringBuilder result = new();
+            var minValue = Math.Pow(-2, 31);
+            var maxValue = Math.Pow(2, 31) - 1;
+
+            while (x != 0)
+            {
+                if (x < 0)
+                {
+                    result.Append('-');
+                    x *= -1;
+                }
+                    
+                result.Append(x % 10);
+                x /= 10;
+            }
+
+            var isCorrectlyInt32 = Int32.TryParse(result.ToString(), out var resultInt);
+
+            return !isCorrectlyInt32 
+                ? 0 
+                : resultInt;
+        }
+    }
+}
