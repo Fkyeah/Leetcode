b15929d231955b11aabdc409006e05a9637cbbb4
Fkyeah <dimx63@mail.ru>
2023-07-21 16:54:59 +0300

    solution and tests

---
+++ Problems.Tests/Easy/LengthOfLastWordTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problems.Easy;
+
+namespace Problems.Tests.Easy
+{
+    [TestClass]
+    public class LengthOfLastWordTests
+    {
+        private static LengthOfLastWordSolution _solution;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _solution = new LengthOfLastWordSolution();
+        }
+
+        [DataTestMethod]
+        [DataRow("Hello World", 5)]
+        [DataRow("   fly me   to   the moon  ", 4)]
+        [DataRow("luffy is still joyboy", 6)]
+        public void First_Solution_Tests(string s, int expectedResult)
+        {
+            var actualResult = _solution.LengthOfLastWord(s);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [DataTestMethod]
+        [DataRow("Hello World", 5)]
+        [DataRow("   fly me   to   the moon  ", 4)]
+        [DataRow("luffy is still joyboy", 6)]
+        public void Second_Solution_Tests(string s, int expectedResult)
+        {
+            var actualResult = _solution.LengthOfLastWord2(s);
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
---
+++ Problems/Easy/LengthOfLastWordSolution.cs
@@ -0,0 +1,34 @@
+﻿namespace Problems.Easy
+{
+    public class LengthOfLastWordSolution
+    {
+        public int LengthOfLastWord(string s)
+        {
+            if (s.Length == 0)
+                return 0;
+
+            var reverse = s
+                .Trim()
+                .Reverse();
+
+            var resultLength = 0;
+            foreach (var item in reverse)
+            {
+                if (item == ' ')
+                    return resultLength;
+
+                resultLength++;
+            }
+
+            return resultLength;
+        }
+
+        public int LengthOfLastWord2(string s)
+        {
+            string[] words = s.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            int lengthOfWords = words.Length - 1;
+
+            return words[lengthOfWords].Length;
+        }
+    }
+}
